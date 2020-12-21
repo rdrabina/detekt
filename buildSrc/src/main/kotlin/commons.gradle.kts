b3165bb210b56c29efbe691b973e406572585c93
Artur Bosch <arturbosch@gmx.de>
2020-12-21 18:49:36 +0100

    Specify source and target 1.8 java version for KotlinCompile tasks

--- buildSrc/src/main/kotlin/commons.gradle.kts
+++ buildSrc/src/main/kotlin/commons.gradle.kts
@@ -67,7 +67,10 @@ configure(subprojects.filter { it.name != "detekt-bom" }) {
     }
 
     tasks.withType<KotlinCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+        targetCompatibility = JavaVersion.VERSION_1_8.toString()
         kotlinOptions.jvmTarget = Versions.JVM_TARGET
+        kotlinOptions.languageVersion = "1.4"
         kotlinOptions.freeCompilerArgs = listOf(
             "-progressive",
             "-Xopt-in=kotlin.RequiresOptIn"
